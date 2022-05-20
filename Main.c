11810b932642385d8c0c50962cbd7befff011d77
Jeonghoon Lee <51705815+2jeonghoon@users.noreply.github.com>
2022-05-20 14:47:09 +0900

    Update Main.c

--- Main.c
+++ Main.c
@@ -39,7 +39,7 @@ typedef struct SelectLinkedList {
 typedef struct TreeNode {
 	TextLinkedList* head;
 	SelectLinkedList* head;
-	int currentindex;
+	int maxindex;
 	struct TreeNode* left, * right;
 }TreeNode;
 
